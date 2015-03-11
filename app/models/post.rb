922c4a311b70d334d6a190502666cd09b7148e16
bgrove1974 <bgrove1974@gmail.com>
2015-03-11 12:08:19 -0700

    Add validation to Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  validates :blurb, :length => { :in => 5..255 }, :format => { :with => /^\A[a-zA-Z\d ]+\z/,
+                    :message => "only allows letters", :multiline => true}
+  validates :title, :body, :presence => true
 end
