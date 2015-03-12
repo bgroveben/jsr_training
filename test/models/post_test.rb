9aa6b7f49b76107a55328e0a1e349af41d14fe07
bgrove1974 <bgrove1974@gmail.com>
2015-03-11 21:27:12 -0700

    More tests for the Posts model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save post without a title" do
+    post = Post.new
+    assert !post.save
+  end
+
+  test "post values should not be empty or invalid" do
+    post = Post.new
+    assert post.invalid?
+    assert post.errors[:title].any?
+    assert post.errors[:blurb].any?
+    assert post.errors[:body].any?
+  end
+
 end
