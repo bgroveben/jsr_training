95718e2c9bb92852a056556c46720ec1f7edd92e
bgrove1974 <bgroveben@gmail.com>
2015-04-09 09:33:50 -0700

    Create callback called set_page for the actions listed in the array

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,15 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_page
+      @page = Page.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def page_params
+      params.require(:page).permit(:title, :slug, :content)
+    end
 end
