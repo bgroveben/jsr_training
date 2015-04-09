07928ba68472b0ee813e34b3260e9d76d5dd6da1
bgrove1974 <bgroveben@gmail.com>
2015-04-09 09:20:12 -0700

    Add callback to pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :set_page, only: [:show, :edit, :update, :destroy]
+
   def index
     @notice = "This came from the pages controller"
   end
